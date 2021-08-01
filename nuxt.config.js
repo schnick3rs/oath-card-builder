2daea524ca0df1a4add7684401e82fafd0bf2ced
schnick3rs <schnickers@gmx.net>
2021-08-01 20:51:43 +0200

    add meta for card builder

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,12 +3,14 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - oath-card-builder',
-    title: 'oath-card-builder',
+    titleTemplate: '%s | Oath Card Builder',
+    title: 'Home',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'robots', name: 'robots', content: 'index,follow' },
+      { hid: 'description', name: 'description', content: 'Design and print your own Denizen, Relics, Sites and Edifices for Oath from Leder Games.' },
+      { hid: 'theme-color', name: 'theme-color', content: '#f69943' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
--- pages/share/_import.vue
+++ pages/share/_import.vue
@@ -14,16 +14,40 @@ const RelicCardWrapper = () => import( /* webpackChunkName: "RelicCardWrapper" *
 const SiteCardWrapper = () => import( /* webpackChunkName: "SiteCardWrapper" */ '~/components/SiteCardWrapper.vue' );
 const EdificeCardWrapper = () => import( /* webpackChunkName: "EdificeCardWrapper" */ '~/components/EdificeCardWrapper.vue' );
 
+const s = (key) => {
+  const map = {
+    'battleplan-both': 'Battleplan',
+    'battleplan-attacker': 'Battleplan',
+    'battleplan-defender': 'Battleplan',
+  };
+  return map[key] || key;
+}
+
 export default {
   name: "_import",
   asyncData({ params }) {
     const importString = params.import;
     const cardJson = decodeURIComponent(escape(Buffer.from(importString, 'base64').toString('ascii')));
-    console.info(cardJson)
     const card = JSON.parse(cardJson);
+
     return {
       card,
     }
+  },
+  head() {
+    const title = `${this.card.name} - a custom ${this.card.__type} card`;
+    const description = `${s(this.card.type).toUpperCase()} > ${this.card.text}`;
+    const image = `/img/${this.card.__type} card back.png`;
+    return {
+      title,
+      meta: [
+        { hid: 'description', name: 'description', content: description },
+        // Open Graph
+        { hid: 'og:title', name: 'og:title', content: title },
+        { hid: 'og:description', name: 'og:description', content: description },
+        { hid: 'og:image', name: 'og:image', content: image },
+      ],
+    };
   },
   methods: {
     dynamicCard(cardType) {
