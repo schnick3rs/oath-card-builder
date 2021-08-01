5f3fc448df29ee21ebbc9cf59e227e91428a80f6
schnick3rs <schnickers@gmx.net>
2021-08-02 01:28:48 +0200

    add canvas printing server side

--- api/routes/canvasService.js
+++ api/routes/canvasService.js
@@ -38,6 +38,7 @@ async function drawSite(card) {
   capitalText(ctx, card.name, 30, 110, 335);
 
   // capacity
+  ctx.font = '30px OathText';
   ctx.fillText(card.capacity,  410, 60);
 
   let resources = card.resources.split('');
@@ -50,6 +51,46 @@ async function drawSite(card) {
   for(let i = 0; i < card.relics; i++) {
     const x = 5*F + 6*F*i + (resources.length*20);
     ctx.drawImage(relic, x, 8*F, 5*F, 5*F);
+  }
+
+  const relicRecovery = await loadImage('https://oath-card-builder.herokuapp.com/img/icons/relic-recover.png');
+  const shared = await loadImage('https://oath-card-builder.herokuapp.com/img/icons/to-favor-bank.png');
+
+  const favorBurned = await loadImage('https://oath-card-builder.herokuapp.com/img/icons/favor-burned.png');
+  const secretBurned = await loadImage('https://oath-card-builder.herokuapp.com/img/icons/secret-burned.png');
+
+  const arcane = await loadImage('https://oath-card-builder.herokuapp.com/img/icons/suit-arcane.png');
+  const order = await loadImage('https://oath-card-builder.herokuapp.com/img/icons/suit-order.png');
+  const beast = await loadImage('https://oath-card-builder.herokuapp.com/img/icons/suit-beast.png');
+  const discord = await loadImage('https://oath-card-builder.herokuapp.com/img/icons/suit-discord.png');
+  const nomad = await loadImage('https://oath-card-builder.herokuapp.com/img/icons/suit-nomad.png');
+  const clockwork = await loadImage('https://oath-card-builder.herokuapp.com/img/icons/suit-clockwork.png');
+  const hearth = await loadImage('https://oath-card-builder.herokuapp.com/img/icons/suit-hearth.png');
+
+  if (card.relicRecoverCost) {
+    let costs = card.relicRecoverCost.length;
+    let startX = 460 - 10*F - (costs * 5 * F);
+    let delta = 0;
+    card.relicRecoverCost.split('').forEach((c, i) => {
+      const x = startX + 10*F - 3*F;
+      switch (c) {
+        case '!': delta += 3*F; ctx.drawImage(favorBurned, x+delta, 356-(11*F), 5*F, 5*F); break;
+        case '@': delta += 3*F; ctx.drawImage(secretBurned, x+delta, 356-(11*F), 5*F, 5*F); break;
+        case '1': delta += 3*F; ctx.drawImage(favor, x+delta, 356-(11*F), 5*F, 5*F); break;
+        case '2': delta += 3*F; ctx.drawImage(secret, x+delta, 356-(11*F), 5*F, 5*F); break;
+        case '>':
+        case '=':
+          delta += 6*F; ctx.drawImage(shared, x+delta, 356-(11*F), 5*F, 5*F); break;
+        case 'a': delta += 6*F; ctx.drawImage(arcane, x+delta, 356-(11*F), 5*F, 5*F); break;
+        case 'c': delta += 6*F; ctx.drawImage(clockwork, x+delta, 356-(11*F), 5*F, 5*F); break;
+        case 'o': delta += 6*F; ctx.drawImage(order, x+delta, 356-(11*F), 5*F, 5*F); break;
+        case 'b': delta += 6*F; ctx.drawImage(beast, x+delta, 356-(11*F), 5*F, 5*F); break;
+        case 'n': delta += 6*F; ctx.drawImage(nomad, x+delta, 356-(11*F), 5*F, 5*F); break;
+        case 'h': delta += 6*F; ctx.drawImage(hearth, x+delta, 356-(11*F), 5*F, 5*F); break;
+        case 'd': delta += 6*F; ctx.drawImage(discord, x+delta, 356-(11*F), 5*F, 5*F); break;
+      }
+    });
+    ctx.drawImage(relicRecovery, startX, 356-(12*F), 8*F, 8*F);
   }
 
   return canvas;
