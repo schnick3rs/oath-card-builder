424a6615fa19f98a8f36daa5397a0c9526208c52
schnick3rs <schnickers@gmx.net>
2021-08-04 15:12:47 +0200

    fix state and hydration error

--- pages/index.vue
+++ pages/index.vue
@@ -12,30 +12,35 @@
       <p>Shows all your created denizen.</p>
     </v-col>
 
-    <v-col cols="12">
-      <v-data-table
-        :headers="table.headers"
-        :items="library"
-      >
+    <client-only>
+      <v-col cols="12">
+        <v-data-table
+          :headers="table.headers"
+          :items="library"
+          class="elevation-1"
+        >
 
-        <template v-slot:item.suit="{ item }">
-          <v-avatar :size="24"><img :src="`/img/icons/suit-${item.suit}.png`"></v-avatar>
-        </template>
+          <template v-slot:item.suit="{ item }">
+            <v-avatar :size="24">
+              <img :alt="item.suit" :src="`/img/icons/suit-${item.suit}.png`" />
+            </v-avatar>
+          </template>
 
-        <template v-slot:item.cost="{ item }">
-          <div
-            v-if="item.cost"
-            style="display: flex; align-content: center; flex-direction: row; justify-content: center; align-items: center;"
-          >
-            <oath-symbol v-for="(c, i) in item.cost" :key="i" :symbol="c" :size="16" />
-          </div>
-        </template>
+          <template v-slot:item.cost="{ item }">
+            <div
+              v-if="item.cost"
+              style="display: flex; align-content: center; flex-direction: row; justify-content: center; align-items: center;"
+            >
+              <oath-symbol v-for="(c, i) in item.cost" :key="i" :symbol="c" :size="16" />
+            </div>
+          </template>
 
-        <template v-slot:item.text="{ item }">
-          <symbol-text :html="formatText(item.text)"></symbol-text>
-        </template>
-      </v-data-table>
-    </v-col>
+          <template v-slot:item.text="{ item }">
+            <symbol-text :html="formatText(item.text)"></symbol-text>
+          </template>
+        </v-data-table>
+      </v-col>
+    </client-only>
 
   </v-row>
 
@@ -57,12 +62,13 @@ export default {
           { text: 'Cost', value: 'cost' },
           { text: 'Text', value: 'text' },
         ]
-      }
+      },
+      data: undefined,
     };
   },
   computed: {
     library() {
-      return this.$store.getters['library/cardSets'] || [];
+      return this.$store.getters['library/cardSets'];
     },
   },
   methods: {
