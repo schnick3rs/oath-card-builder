558de83664c4b3998222aebb59e727267d9747d0
schnick3rs <schnickers@gmx.net>
2022-11-07 17:01:15 +0100

    remove colors

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
@@ -81,16 +80,6 @@ export default {
     theme: {
       //dark: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
     }
   },
 
